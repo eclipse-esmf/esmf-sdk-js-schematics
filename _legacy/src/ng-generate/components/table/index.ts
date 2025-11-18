54b489d28682e8642086202705966e46455da6f9
Anton Shabatin <anton.shabatin@bosch.com>
2025-11-18 14:21:53 +0100

    Setup nx workspace
    Description:
    - Moved the current implementation to _legacy folder
    - Initialized nx workspace from scratch
      - Angular 20.x
      - Jest
      - Playwright
    - Added demo application
    - Setup all the commands to run demo application and all the quality checks

---
+++ _legacy/src/ng-generate/components/table/index.ts
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2024 Robert Bosch Manufacturing Solutions GmbH
+ *
+ * See the AUTHORS file(s) distributed with this work for
+ * additional information regarding authorship.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import {chain, Rule, SchematicContext} from '@angular-devkit/schematics';
+import {Tree} from '@angular-devkit/schematics/src/tree/interface';
+import {
+    addAndUpdateConfigurationFilesRule,
+    formatAllFilesRule,
+    generateComponent,
+    generateGeneralFilesRules,
+    insertVersionIntoPathRule,
+    insertVersionIntoSelectorRule,
+    loadAspectModelRule,
+    loadRdfRule,
+    options,
+    prepareOptions,
+    setComponentNameRule,
+    setCustomActionsAndFiltersRule,
+    setTemplateOptionValuesRule,
+} from '../shared/index';
+import {ComponentType} from '../shared/schema';
+import {generateStorageService} from './generators/services/storage/index';
+import {generateColumnMenu} from './generators/components/column-menu/index';
+import {generateConfigMenu} from './generators/components/config-menu/index';
+import {generateResizeDirective} from './generators/directives/resize/index';
+import {generateHighlightDirective} from './generators/directives/highlight/index';
+import {generateTableComponent} from './generators/components/table/index';
+import {generateDataSource} from './generators/data-source/index';
+import {TableSchema} from './schema';
+import {generateExportTableDialog} from './generators/components/export-dialog/index';
+import {generatePaginatorSelectConfigProvider} from '../shared/generators/index';
+import {generateTableCellTooltipDirective} from './generators/directives/table-cell-tooltip/index';
+import {generateTableCellComponent} from './generators/components/table-cell/index';
+import {generateTableCellLinkComponent} from './generators/components/table-cell-link/index';
+
+export default function (tableSchema: TableSchema): Rule {
+    return (tree: Tree, context: SchematicContext) => {
+        generateComponent(context, tableSchema, ComponentType.TABLE);
+    };
+}
+
+export function generateTable(tableSchema: TableSchema): Rule {
+    prepareOptions(tableSchema, ComponentType.TABLE);
+
+    return chain([
+        loadRdfRule(),
+        loadAspectModelRule(),
+        setCustomActionsAndFiltersRule(),
+        setComponentNameRule(ComponentType.TABLE),
+        insertVersionIntoSelectorRule(),
+        insertVersionIntoPathRule(),
+        setTemplateOptionValuesRule(),
+        ...generateGeneralFilesRules(),
+        ...tableSpecificGeneration(),
+        ...addAndUpdateConfigurationFilesRule(),
+        formatAllFilesRule(),
+    ]);
+}
+
+function tableSpecificGeneration(): Array<Rule> {
+    return [
+        generateTableComponent(options),
+        generateTableCellComponent(options), // General
+        generateTableCellLinkComponent(options), // General
+        generateDataSource(options),
+        generateStorageService(options), // General
+        generateColumnMenu(options), // General
+        generateConfigMenu(options), // General
+        generateExportTableDialog(options), // General
+        generatePaginatorSelectConfigProvider(options),
+        generateResizeDirective(options), // General
+        generateHighlightDirective(options), // General
+        generateTableCellTooltipDirective(options), // General
+    ];
+}
---
+++ apps/demo/src/app/app.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('ESMF SDK JS Schematic Demo');
+  });
+});
