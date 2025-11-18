209a95dca453836208094394cc8e18fa8f82a465
Anton Shabatin <anton.shabatin@bosch.com>
2025-11-18 14:51:01 +0100

    Add @esmf/semantic-ui-schematic library scaffold

---
+++ packages/schematic/src/lib/schematic/schematic.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'lib-schematic',
+  imports: [],
+  templateUrl: './schematic.html',
+  styleUrl: './schematic.css',
+})
+export class Schematic {}
