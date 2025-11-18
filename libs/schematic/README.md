4a23e937246c0f8d913b1d61eb59e3984743fa05
Anton Shabatin <anton.shabatin@bosch.com>
2025-11-18 16:22:39 +0100

    Add @esmf/semantic-ui-schematic library scaffold

---
+++ libs/schematic/README.md
@@ -0,0 +1,7 @@
+# schematic
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test schematic` to execute the unit tests.
---
+++ libs/schematic/src/lib/schematic/schematic.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'lib-schematic',
+  imports: [],
+  templateUrl: './schematic.html',
+  styleUrl: './schematic.scss',
+})
+export class Schematic {}
