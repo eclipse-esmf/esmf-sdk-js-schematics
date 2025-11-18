33ea4418f7d43850e0a98a2aadda2a08bd5f5d6e
Anton Shabatin <anton.shabatin@bosch.com>
2025-11-18 14:42:55 +0100

    Add @esmf/semantic-ui-components library scaffold

---
+++ packages/components/src/lib/components/components.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'lib-components',
+  imports: [],
+  templateUrl: './components.html',
+  styleUrl: './components.css',
+})
+export class Components {}
