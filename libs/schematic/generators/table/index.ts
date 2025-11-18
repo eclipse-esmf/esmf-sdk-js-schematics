159e58ddf4ddfa9ad1784b2543936a2cb5a0feb0
Anton Shabatin <anton.shabatin@bosch.com>
2025-11-18 17:15:45 +0100

    Add table generator
    Description
    The table generator creates an example file ba running the command
    - nx command - nx g @esmf/semantic-ui-schematic:table --name=test --project=demo
    - ng command - npm run ng -- g @esmf/semantic-ui-schematic:table --name=test --project=demo

---
+++ libs/schematic/generators/table/index.ts
@@ -0,0 +1,8 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+export default function (options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    tree.create(`my-file.txt`, `Hello, ${options.name}!`);
+    return tree;
+  };
+}
