127ca949a7f3a2fc6a033adac4afc032b136d1cd
Anton Shabatin <anton.shabatin@bosch.com>
2025-11-27 19:23:06 +0100

    Make ChipList component generic

---
+++ libs/schematic/src/lib/components/chip-list/chip-list.component.ts
@@ -0,0 +1,36 @@
+import {Component, EventEmitter, input, Output, ViewEncapsulation} from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatTooltip} from '@angular/material/tooltip';
+import {FilterEnums, FilterType} from '../../models/filter.definition';
+import {MatMiniFabButton} from '@angular/material/button';
+import {MatChipListbox, MatChipOption} from '@angular/material/chips';
+import {HorizontalOverflowDirective} from '../../directives/horizontal-overflow.directive';
+
+@Component({
+  selector: 'esmf-chip-list',
+  templateUrl: './chip-list.component.html',
+  imports: [MatIcon, MatTooltip, MatMiniFabButton, MatChipListbox, MatChipOption, HorizontalOverflowDirective],
+  styleUrls: ['./chip-list.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: { class: 'esmf-chip-list' }
+})
+export class EsmfChipListComponent {
+  activeFilters = input.required<FilterType[]>();
+  @Output() removeFilter = new EventEmitter<FilterType>();
+
+  triggerRemoveFilter(filter: FilterType): void {
+    this.removeFilter.emit(filter);
+  }
+
+  chipListValue(filter: FilterType): string {
+    if (filter.type === FilterEnums.Search) {
+      return `${filter.filterValue}: ${filter.label}`;
+    }
+
+    if (filter.type === FilterEnums.Date) {
+      return filter.label;
+    }
+
+    return `${filter.prop}: ${filter.label}`;
+  }
+}
