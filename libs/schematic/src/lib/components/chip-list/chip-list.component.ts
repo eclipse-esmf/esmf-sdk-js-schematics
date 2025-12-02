c8f8ab6673cae4c4679f020fb560c972d7a32da8
Anton Shabatin <anton.shabatin@bosch.com>
2025-12-02 14:54:34 +0100

    Extract RQL Extension interfaces from table generation

--- libs/schematic/src/lib/components/chip-list/chip-list.component.ts
+++ libs/schematic/src/lib/components/chip-list/chip-list.component.ts
@@ -5,11 +5,12 @@ import {FilterEnums, FilterType} from '../../models/filter.definition';
 import {MatMiniFabButton} from '@angular/material/button';
 import {MatChipListbox, MatChipOption} from '@angular/material/chips';
 import {EsmfHorizontalOverflowDirective} from '../../directives/horizontal-overflow.directive';
+import {TranslocoDirective} from '@jsverse/transloco';
 
 @Component({
   selector: 'esmf-chip-list',
   templateUrl: './chip-list.component.html',
-  imports: [MatIcon, MatTooltip, MatMiniFabButton, MatChipListbox, MatChipOption, EsmfHorizontalOverflowDirective],
+  imports: [MatIcon, MatTooltip, MatMiniFabButton, MatChipListbox, MatChipOption, EsmfHorizontalOverflowDirective, TranslocoDirective],
   styleUrls: ['./chip-list.component.scss'],
   encapsulation: ViewEncapsulation.None,
   host: {class: 'esmf-chip-list'},
