6c6d31cf32539ef38dd0f251b16081fc3fb5811d
Anton Shabatin <anton.shabatin@bosch.com>
2025-12-27 17:53:29 +0100

    Init translocoat demo app

--- apps/demo/src/app/transloco-http-loader.service.ts
+++ apps/demo/src/app/transloco-http-loader.service.ts
@@ -1,12 +1,16 @@
 import {inject, Injectable} from '@angular/core';
 import {Translation, TranslocoLoader} from '@jsverse/transloco';
 import {HttpClient} from '@angular/common/http';
+import {forkJoin, map} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return forkJoin({
+      app: this.http.get<Translation>(`/assets/i18n/${lang}.json`),
+      vendor: this.http.get<Translation>(`/assets/i18n/${lang}.vendor.json`),
+    }).pipe(map(({vendor, app}) => ({...vendor, ...app})));
   }
 }
